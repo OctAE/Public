8b25be8b1591392718d1ea8f49bd68efae783a56
OctAE <no-reply@octink.com>
2016-10-03 15:34:01 +0100

    update

--- TW-jquery-functions.js
+++ TW-jquery-functions.js
@@ -385,7 +385,7 @@ $(document).ready(function(){
 
     	if ($('a#addToKit span:contains(kit)').length == 0) {
 		      
-		        var breadcrumbHtml = "<div style='padding: 10px 0px 30px 4px; display: block'><strong>You are at: > > <a href='/taylorwimpey/products'>Tylor Wimpey</a>"
+		        var breadcrumbHtml = "<div style='padding: 10px 0px 30px 4px; display: block'><strong>Current order status: > > <a href='/taylorwimpey/products'>Tylor Wimpey</a>"
 		       	var pathArray = pathname.split("/");
 		        var pathBefore = "";
 		    		for (i = 2; i < pathArray.length-1; i++) {
