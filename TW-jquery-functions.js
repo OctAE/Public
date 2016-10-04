e3a8751a7cc51f7ff1e535bc05687be2b4048804
OctAE <no-reply@octink.com>
2016-10-04 11:09:32 +0100

    update

--- TW-jquery-functions.js
+++ TW-jquery-functions.js
@@ -122,15 +122,14 @@ $(document).ready(function(){
 	 
 	//Remove prefix for product order
 	//Change your are at text to the below
-	$('.productGridTD > div > span').each(function() {
+	$('div.centrealign span').each(function() {
     	var text = $(this).text();
-    	$(this).text(text.replace('01. Standard Double/Triple Garage', 'Standard Double/Triple Garage')); 
-    	$(this).text(text.replace('02. Standard House', 'Standard House')); 
-    	$(this).text(text.replace('03. Standard Cabin (Standard fascia)', 'Standard Cabin (Standard fascia)')); 
-    	$(this).text(text.replace('04. Standard Cabin (Retractable fascia)', 'Standard Cabin (Retractable fascia)')); 
-    	$(this).text(text.replace('', '')); 
-    	$(this).text(text.replace('', '')); 
-
+    	$(this).text(text.replace('01. Standard Double/Triple Garage', 'Standard Double/Triple Garage'));
+    	$(this).text(text.replace('02. Standard House', 'Standard House'));
+    	$(this).text(text.replace('03. Standard Cabin (Standard fascia)', 'Standard Cabin (Standard fascia)'));
+    	$(this).text(text.replace('04. Standard Cabin (Retractable fascia)', 'Standard Cabin (Retractable fascia)'));
+    	$(this).text(text.replace('', ''));
+    	$(this).text(text.replace('', ''));
 	});
 
 
